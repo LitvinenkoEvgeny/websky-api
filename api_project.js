4994e0098bc92e97c7ffb9946e59de3c915dbe0a
LitvinenkoEvgeny <yourbisnesspartner@gmail.com>
2018-10-16 18:53:43 +0400

    generate

--- api_project.js
+++ api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-10-16T14:31:18.271Z",
+    "time": "2018-10-16T14:53:28.282Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
